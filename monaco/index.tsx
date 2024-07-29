40366b6ca9ad4592e6a8f5c87a7f05e73bde46ac
Geisson Machado Antunes <geisonnm@gmail.com>
2024-07-29 10:41:12 -0300

    groups api

---
+++ monaco/index.tsx
@@ -0,0 +1,33 @@
+// src/MonacoEditor.tsx
+import MonacoEditor, { Monaco, OnChange } from "@monaco-editor/react";
+import React from "react";
+
+interface MonacoEditorProps {
+  code: string;
+  onChange: OnChange;
+}
+
+const MonacoEditorComponent: React.FC<MonacoEditorProps> = ({ code = {} }) => {
+  const handleEditorWillMount = (monaco: Monaco) => {
+    // Configurando o Monaco para o modo JSON
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      allowComments: false,
+      schemas: [],
+    });
+  };
+
+  return (
+    <MonacoEditor
+      height="500px"
+      language="json"
+      value={code}
+      beforeMount={handleEditorWillMount}
+      onChange={() => {
+        console.log("changed");
+      }}
+    />
+  );
+};
+
+export default MonacoEditorComponent;
